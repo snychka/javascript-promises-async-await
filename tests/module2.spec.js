23f9892777d34e9694e541f56d25be49176ae3e1
Marques Woodson <mwoodson@project44.com>
2020-03-04 21:02:28 -0600

    remove the index.js file

--- tests/module2.spec.js
+++ tests/module2.spec.js
@@ -18,11 +18,18 @@ describe("Module 2", () => {
       "utf8"
     );
     const res = acorn.parse(indexFile, { sourceType: "module" });
-
     const functionDeclaration = res.body.filter(
       node => _.get(node, "declaration.id.name") === "fetchMovies"
     )[0];
-    expect(_.get(functionDeclaration, "declaration.id.name", "")).to.equal(
+    const variableDeclaration = res.body.filter(
+      node =>
+        _.get(node, "declaration.type", "") === "VariableDeclaration" &&
+        _.get(node, "declaration.declarations[0].id.name", "") === "fetchMovies"
+    )[0];
+    expect(
+      _.get(functionDeclaration, "declaration.id.name", "") ||
+        _.get(variableDeclaration, "declaration.declarations[0].id.name", "")
+    ).to.equal(
       "fetchMovies",
       "It seems that you have the wrong function name. It should be `fetchMovies`"
     );
@@ -37,50 +44,105 @@ describe("Module 2", () => {
     const functionDeclaration = res.body.filter(
       node => _.get(node, "declaration.id.name") === "fetchWithTimeout"
     )[0];
-    const functionBody = functionDeclaration.declaration.body.body;
-    const body = functionBody[0];
+    const variableDeclaration = res.body.filter(
+      node =>
+        _.get(node, "declaration.type", "") === "VariableDeclaration" &&
+        _.get(node, "declaration.declarations[0].id.name", "") ===
+          "fetchWithTimeout"
+    )[0];
 
-    expect(_.get(functionDeclaration, "declaration.id.name", "")).to.equal(
-      "fetchWithTimeout",
-      "It seems that you have the wrong function name. It should be `fetchWithTimeout`"
-    );
+    if (variableDeclaration) {
+      body = variableDeclaration.declaration.declarations[0].init.body.body[0];
+      expect(
+        _.get(variableDeclaration, "declaration.declarations[0].id.name", "")
+      ).to.equal(
+        "fetchWithTimeout",
+        "It seems that you have the wrong function name. It should be `fetchWithTimeout`"
+      );
+      expect(_.get(body, "type", "")).to.equal(
+        "ReturnStatement",
+        "You need to have a return in the `fetchWithTimeout` function"
+      );
 
-    expect(_.get(body, "type", "")).to.equal(
-      "ReturnStatement",
-      "You need to have a return in the `fetchWithTimeout` function"
-    );
+      expect(_.get(body, "argument.type", "")).to.equal(
+        "NewExpression",
+        "It looks like you are missing the `new` keyword when declaring the Promise"
+      );
+      expect(_.get(body, "argument.callee.name", "")).to.equal(
+        "Promise",
+        "fetchWithTimeout needs to return a `new Promise`"
+      );
+      expect(_.get(body, "argument.arguments[0].params[0].name", "")).to.equal(
+        "resolve",
+        "It looks like you are missing the `resolve` parameter of your Promise callback."
+      );
 
-    expect(_.get(body, "argument.type", "")).to.equal(
-      "NewExpression",
-      "It looks like you are missing the `new` keyword when declaring the Promise"
-    );
-    expect(_.get(body, "argument.callee.name", "")).to.equal(
-      "Promise",
-      "fetchWithTimeout needs to return a `new Promise`"
-    );
-    expect(_.get(body, "argument.arguments[0].params[0].name", "")).to.equal(
-      "resolve",
-      "It looks like you are missing the `resolve` parameter of your Promise callback."
-    );
+      expect(
+        _.get(body, "argument.arguments[0].body.callee.name", "")
+      ).to.equal(
+        "setTimeout",
+        "The Promise callback should invoke `setTimeout`."
+      );
 
-    expect(_.get(body, "argument.arguments[0].body.callee.name", "")).to.equal(
-      "setTimeout",
-      "The Promise callback should invoke `setTimeout`."
-    );
+      expect(
+        _.get(body, "argument.arguments[0].body.arguments[0].name", "")
+      ).to.equal(
+        "resolve",
+        "`setTimeout` should have the variable `resolve` as the first param."
+      );
 
-    expect(
-      _.get(body, "argument.arguments[0].body.arguments[0].name", "")
-    ).to.equal(
-      "resolve",
-      "`setTimeout` should have the variable `resolve` as the first param."
-    );
+      expect(
+        _.get(body, "argument.arguments[0].body.arguments[1].name", "")
+      ).to.equal(
+        "delay",
+        "`setTimeout` should have the variable `delay` as the second param."
+      );
+    } else {
+      body = functionDeclaration.declaration.body.body[0];
+      expect(_.get(functionDeclaration, "declaration.id.name", "")).to.equal(
+        "fetchWithTimeout",
+        "It seems that you have the wrong function name. It should be `fetchWithTimeout`"
+      );
 
-    expect(
-      _.get(body, "argument.arguments[0].body.arguments[1].name", "")
-    ).to.equal(
-      "delay",
-      "`setTimeout` should have the variable `delay` as the second param."
-    );
+      expect(_.get(body, "type", "")).to.equal(
+        "ReturnStatement",
+        "You need to have a return in the `fetchWithTimeout` function"
+      );
+
+      expect(_.get(body, "argument.type", "")).to.equal(
+        "NewExpression",
+        "It looks like you are missing the `new` keyword when declaring the Promise"
+      );
+      expect(_.get(body, "argument.callee.name", "")).to.equal(
+        "Promise",
+        "fetchWithTimeout needs to return a `new Promise`"
+      );
+      expect(_.get(body, "argument.arguments[0].params[0].name", "")).to.equal(
+        "resolve",
+        "It looks like you are missing the `resolve` parameter of your Promise callback."
+      );
+
+      expect(
+        _.get(body, "argument.arguments[0].body.callee.name", "")
+      ).to.equal(
+        "setTimeout",
+        "The Promise callback should invoke `setTimeout`."
+      );
+
+      expect(
+        _.get(body, "argument.arguments[0].body.arguments[0].name", "")
+      ).to.equal(
+        "resolve",
+        "`setTimeout` should have the variable `resolve` as the first param."
+      );
+
+      expect(
+        _.get(body, "argument.arguments[0].body.arguments[1].name", "")
+      ).to.equal(
+        "delay",
+        "`setTimeout` should have the variable `delay` as the second param."
+      );
+    }
   });
 
   it("you should have a file named 'services.js' in the src directory @services", () => {
@@ -101,17 +163,42 @@ describe("Module 2", () => {
     const functionDeclaration = res.body.filter(
       node => _.get(node, "declaration.id.name") === "fetchWithTimeout"
     )[0];
-    const functionParams = functionDeclaration.declaration.params;
-    const params = functionParams[0];
 
-    expect(_.get(functionDeclaration, "declaration.id.name", "")).to.equal(
-      "fetchWithTimeout",
-      "It seems that you have the wrong function name. It should be `fetchWithTimeout`"
-    );
+    const variableDeclaration = res.body.filter(
+      node =>
+        _.get(node, "declaration.type", "") === "VariableDeclaration" &&
+        _.get(node, "declaration.declarations[0].id.name", "") ===
+          "fetchWithTimeout"
+    )[0];
 
-    expect(_.get(params, "name", "")).to.equal(
-      "delay",
-      "It looks like you are missing the `delay` parameter."
-    );
+    if (variableDeclaration) {
+      const functionParams =
+        variableDeclaration.declaration.declarations[0].init.params;
+      const params = functionParams[0];
+      expect(
+        _.get(variableDeclaration, "declaration.declarations[0].id.name", "")
+      ).to.equal(
+        "fetchWithTimeout",
+        "It seems that you have the wrong function name. It should be `fetchWithTimeout`"
+      );
+
+      expect(_.get(params, "name", "")).to.equal(
+        "delay",
+        "It looks like you are missing the `delay` parameter."
+      );
+    } else {
+      const functionParams = functionDeclaration.declaration.params;
+      const params = functionParams[0];
+
+      expect(_.get(functionDeclaration, "declaration.id.name", "")).to.equal(
+        "fetchWithTimeout",
+        "It seems that you have the wrong function name. It should be `fetchWithTimeout`"
+      );
+
+      expect(_.get(params, "name", "")).to.equal(
+        "delay",
+        "It looks like you are missing the `delay` parameter."
+      );
+    }
   });
 });
