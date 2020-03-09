47111b89c11c1dd87e4fa76858bb3794bf472be1
Marques Woodson <mwoodson@project44.com>
2020-03-09 07:45:37 -0500

    adding tests for module 7

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
+import {
+  fetchWithTimeout,
+  fetchMovies,
+  fetchBooks,
+  asyncFetchBooks,
+  asyncFetchMovies
+} from "./services";
 const movies = require("./data/movies.json");
 
 const getBooksAndMovies = () => {
@@ -26,3 +32,47 @@ const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {
   console.log("getBooksOrMoviesPromise", results);
 });
+
+async function getBooksAndMoviesAsync() {
+  try {
+    const [books, movies] = await Promise.all([
+      asyncFetchBooks(),
+      asyncFetchMovies()
+    ]);
+    return {
+      books,
+      movies
+    };
+  } catch (error) {
+    console.log("Error fetching books and movies", error);
+  }
+}
+
+async function getBooksOrMoviesAsync() {
+  try {
+    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+    return values;
+  } catch (error) {
+    console.error("Error waiting for the promise race", error);
+    throw error;
+  }
+}
+
+getBooksAndMoviesAsync()
+  .then(results => {
+    console.log("movies AND books", {
+      movies: results.movies,
+      books: results.books
+    });
+  })
+  .catch(error => {
+    console.error("Error in getBooksAndMoviesAsync execution", error);
+  });
+
+getBooksOrMoviesAsync()
+  .then(results => {
+    console.log("movies OR books", {
+      results
+    });
+  })
+  .catch(err => console.error(err));
