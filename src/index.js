1709b07f6762bce11de124a33f1434b2a404d8d6
snychka <stefannychka@gmail.com>
2020-03-16 06:25:14 -0600

    failed attempt to fix module3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ let movies = require('./data/movies.json');
 export function fetchMovies() {
  const resolveFunction = () => movies;
  return fetchWithTimeout(1000).then(resolveFunction);
- return new Promise(resolve => setTimeout(resolve, delay)); 
 }
 
 let moviePromise = fetchMovies();
