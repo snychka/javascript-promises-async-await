8cb878107019d3bd86f87e051278611135689d4f
snychka <stefannychka@gmail.com>
2020-03-16 06:58:32 -0600

    module4, task 8 & 10 do not pass

--- src/services.js
+++ src/services.js
@@ -1,3 +1,18 @@
 export function fetchWithTimeout(delay) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
+
+export function fetchMovies() { 
+//return fetch("/data/movies.json")
+return fetch("./data/movies.json")
+  .then(response => response.json())
+  .then(movies => movies)
+  .catch(error => console.log(error));
+}
+export function fetchBooks() { 
+  //return fetch("/data/books.json")
+  return fetch("./data/books.json")
+  .then(response => response.json())
+  .then(books => books)
+  .catch(error => console.log(error));
+}
