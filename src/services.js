9ad430412efde73d18e527289b5738e7e8062474
snychka <stefannychka@gmail.com>
2020-03-22 19:26:48 -0600

    finish module5

--- src/services.js
+++ src/services.js
@@ -16,3 +16,13 @@ export function fetchBooks() {
   .then(books => books)
   .catch(error => console.log(error));
 }
+
+export async function asyncFetchMovies() {
+  try {
+    const response = await fetch("/data/movies.json");
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+}
